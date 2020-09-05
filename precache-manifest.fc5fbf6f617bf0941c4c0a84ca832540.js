a35ca9820f664698985d8d9c037db0a058472649
Arpit Jindal <61323807+arpitdhn1998@users.noreply.github.com>
2020-09-05 17:07:46 +0530

    Updates

---
+++ precache-manifest.fc5fbf6f617bf0941c4c0a84ca832540.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2095d95f069a95dd6f33875e105afe",
+    "url": "/Business_Tech/index.html"
+  },
+  {
+    "revision": "2ef15a6ba9c29bc91659",
+    "url": "/Business_Tech/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "66b1d3e1a3d1d25c654f",
+    "url": "/Business_Tech/static/css/main.563c7a92.chunk.css"
+  },
+  {
+    "revision": "2ef15a6ba9c29bc91659",
+    "url": "/Business_Tech/static/js/2.fb06489a.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/Business_Tech/static/js/2.fb06489a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b1d3e1a3d1d25c654f",
+    "url": "/Business_Tech/static/js/main.174bc7ac.chunk.js"
+  },
+  {
+    "revision": "0007d633985b2e7123c1",
+    "url": "/Business_Tech/static/js/runtime-main.0ca892ae.js"
+  },
+  {
+    "revision": "fe82dc931938f41e29f6ee899d89b99c",
+    "url": "/Business_Tech/static/media/Abt.fe82dc93.jpg"
+  },
+  {
+    "revision": "81d563a8f9ba2e7c114b3bab2c782c66",
+    "url": "/Business_Tech/static/media/Android.81d563a8.jpg"
+  },
+  {
+    "revision": "ce0de232b178321f21386e604e163288",
+    "url": "/Business_Tech/static/media/App.ce0de232.jpg"
+  },
+  {
+    "revision": "fe34ba39ff19e32ae01a7d2c8435b9f7",
+    "url": "/Business_Tech/static/media/Dig.fe34ba39.jpg"
+  },
+  {
+    "revision": "38cbea4e48225668d1b3ad8edb223fc5",
+    "url": "/Business_Tech/static/media/Onl.38cbea4e.jpg"
+  },
+  {
+    "revision": "239fb326bd662973c1ae6255ed23bc59",
+    "url": "/Business_Tech/static/media/Soft.239fb326.jpg"
+  },
+  {
+    "revision": "3592a8335512c110f3e5eb113605ad52",
+    "url": "/Business_Tech/static/media/Web.3592a833.jpg"
+  }
+]);
